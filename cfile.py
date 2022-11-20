b6f7835b4bf1a93b75cc36f4f11419f0b97810c9
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2022-11-20 16:58:53 +0900

    Update cfile.py

--- cfile.py
+++ cfile.py
@@ -1,21 +1,22 @@
 # 파일 전송 클라이언트
-
 from socket import *
-import os, sys, argparse, subprocess
+import sys, argparse, subprocess
 
 if __name__ == '__main__' :
-    parser = argparse.ArgumentParser(description='python3 cfile.py -file=[file name]')
+    parser = argparse.ArgumentParser(description='python3 cfile.py -f [file name] -i [ip]')
     
     # 입력받을 인자값 등록 required 필수면 True
-    parser.add_argument('-file',required=True, help='-file=파일 이름을 입력해주세요')
+    parser.add_argument('-f',required=True, help='-f 파일 이름을 입력해주세요')
+    parser.add_argument('-i',required=True, help='-i 접속할 아이피를 입력해주세요')
 
     # 입력 받은 인자값을 args에 저장
     args = parser.parse_args()
 
-    filename = args.file
+    filename = args.f
+    ip = args.i
     
     clientSock = socket(AF_INET, SOCK_STREAM)
-    clientSock.connect(('127.0.0.1', 4040))
+    clientSock.connect((ip, 4040))
 
     print('연결에 성공했습니다.')
     clientSock.sendall(filename.encode('utf-8'))
@@ -27,8 +28,7 @@ if __name__ == '__main__' :
         print('파일 %s 가 서버에 존재하지 않음' %filename)
         sys.exit()
 
-    nowdir = os.getcwd()
-    with open(nowdir+"\\"+filename, 'wb') as f: #현재dir에 filename으로 파일을 받는다
+    with open(filename, 'wb') as f: #현재dir에 filename으로 파일을 받는다
         try:
             while data: #데이터가 있을 때까지
                 f.write(data) #1024바이트 쓴다
