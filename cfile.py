962ab3b3ff4384dba38edcebc7168704fbd0f065
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2022-11-20 17:29:11 +0900

    2022-11-20 latest

--- cfile.py
+++ cfile.py
@@ -12,22 +12,23 @@ if __name__ == '__main__' :
     # 입력 받은 인자값을 args에 저장
     args = parser.parse_args()
 
-    filename = args.f
+    filename = args.f # 서버에서 읽을 파일이름
     ip = args.i
     
     clientSock = socket(AF_INET, SOCK_STREAM)
     clientSock.connect((ip, 4040))
 
+    print('파일 이름 : ',filename)
     print('연결에 성공했습니다.')
     clientSock.sendall(filename.encode('utf-8'))
 
     data = clientSock.recv(1024)
     data_transferred = 0
-
     if not data:
         print('파일 %s 가 서버에 존재하지 않음' %filename)
         sys.exit()
-
+    
+    filename = 'screen1.png' # 저장할 파일이름
     with open(filename, 'wb') as f: #현재dir에 filename으로 파일을 받는다
         try:
             while data: #데이터가 있을 때까지
@@ -36,5 +37,6 @@ if __name__ == '__main__' :
                 data = clientSock.recv(1024) #1024바이트를 받아 온다
         except Exception as ex:
             print(ex)
+    filename = 'eog ' + filename
     print('파일 %s 받기 완료. 전송량 %d' %(filename, data_transferred))
-    subprocess.Popen('eog '+ filename, shell=True)
+    subprocess.run(filename, shell=True)
